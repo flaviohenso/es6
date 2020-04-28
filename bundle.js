21cba7410857685266559a74829ab4c39d075c36
Flavio Henrique S Cabral <flavio.henso@gmail.com>
2020-04-28 16:33:02 -0300

    Conceitos

--- bundle.js
+++ bundle.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
@@ -77,3 +87,86 @@ document.getElementById("btnAdd").onclick = function () {
 };
 
 console.log(List.soma(2, 6));
+var arr = [1, 2, 3, 4, 5];
+var newArr = arr.map(function (num) {
+  return num * 2;
+});
+console.log(newArr);
+
+var teste = function teste() {
+  return {
+    nome: 'flavio'
+  };
+};
+
+console.log(teste); //desestruturacao de Objetos
+
+var usuario = {
+  nome: 'flavio',
+  idade: 33,
+  endereco: {
+    cidade: 'Natal',
+    estado: 'RN'
+  }
+};
+console.log(usuario.nome);
+console.log(usuario.endereco.cidade);
+var nome = usuario.nome,
+    idade = usuario.idade,
+    estado = usuario.endereco.estado;
+console.log(nome);
+console.log(estado);
+console.log(idade); // REST
+
+var usuarioR = {
+  name: "flavio",
+  idade: 33,
+  empresa: "flavio henrique"
+};
+
+var name = usuarioR.name,
+    resto = _objectWithoutProperties(usuarioR, ["name"]);
+
+console.log(name);
+console.log(resto);
+var numeros = [1, 2, 3, 4, 5];
+var a = numeros[0],
+    b = numeros[1],
+    c = numeros.slice(2);
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6, 7)); //SPREAD
+
+var arr1 = [1, 2, 3, 4];
+var arr2 = [5, 6, 7, 8];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario2 = {
+  nome: 'flavio',
+  idade: 33,
+  empresa: 'flavio henrique',
+  endereco: {
+    cidade: 'Natal',
+    uf: 'RN'
+  }
+};
+
+var usuario3 = _objectSpread({}, usuario2, {
+  endereco: {
+    cidade: 'sga'
+  }
+});
+
+console.log(usuario3);
--- main.js
+++ main.js
@@ -33,3 +33,82 @@ document.getElementById("btnAdd").onclick = () => {
 }
 
 console.log(List.soma(2,6))
+
+const arr = [1,2,3,4,5]
+
+const newArr = arr.map( num => num * 2 )
+
+console.log(newArr)
+
+const teste = () => ({ nome: 'flavio'})
+
+console.log(teste)
+
+//desestruturacao de Objetos
+
+const usuario = {
+    nome: 'flavio',
+    idade: 33,
+    endereco: {
+        cidade: 'Natal',
+        estado: 'RN'
+    }
+}
+
+console.log(usuario.nome)
+console.log(usuario.endereco.cidade)
+
+const { nome ,idade, endereco: {estado}} = usuario
+
+console.log(nome)
+console.log(estado)
+console.log(idade)
+
+// REST
+const usuarioR = {
+    name: "flavio",
+    idade: 33,
+    empresa: "flavio henrique"
+}
+
+const {name, ...resto} = usuarioR
+
+console.log(name)
+console.log(resto)
+
+const numeros = [1,2,3,4,5]
+
+const [a,b,...c] = numeros
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+function soma(...params){
+    return params.reduce((total,next) => total + next)
+}
+
+console.log(soma(1,2,3,4,5,6,7))
+
+//SPREAD
+
+const arr1 = [1,2,3,4]
+const arr2 = [5,6,7,8]
+
+const arr3 = [...arr1,...arr2]
+
+console.log(arr3)
+
+const usuario2 = {
+    nome: 'flavio',
+    idade: 33,
+    empresa: 'flavio henrique',
+    endereco: {
+        cidade: 'Natal',
+        uf: 'RN'
+    }
+}
+
+const usuario3 = {...usuario2, endereco: {cidade: 'sga'}} 
+
+console.log(usuario3)
